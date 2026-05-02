f8dbd88a6cf79b2f4c2fa32552c722eca098b6a7
samiulmustakim <samiulmustakim99@gmail.com>
2026-05-02 21:21:34 +0600

    Added nav And Banner

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Banner from "@/components/HomeSection/Banner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-9xl mx-auto">
+      <Banner></Banner>
+    </div>
+  );
+}
